b30946ce503f96c893801da9e13c3a2a5f0c6cbd
Allan Laal <allan.laal@gmail.com>
2021-10-29 15:30:48 +0300

    Create run.bash

---
+++ run.bash
@@ -0,0 +1,61 @@
+#!/bin/bash
+# hg 	= 59.066926N 24.849028E
+# saue	= 59.319424N 24.553180E
+
+COORDINATES='59.319424N 24.553180E' # decimal longitude + decimal latitude: examples:
+	#	'12.12345N 56.78954E' 
+	#	'-12.12345N -56.78954E' 
+	#	'12.12345S 56.78954W' 
+SUN_ANGLE=0.53 # sun angle above/below the horizon. examples: 
+	#	  0.5 	sun is visible			Suns lower edge is touching the horizon (the Sun is 0.53° high in our sky)
+	#	  0		twilighty				Suns upper edge is just above the horizon
+	#	 -6		civil twilight			Suns lower edge is 6° below the horizon. 
+	#	-12		nautical twilight		Suns lower edge is 12° below the horizon. 
+	#	-18		astronomical twilight	Suns lower edge is 18° below the horizon. 
+	# see: https://en.wikipedia.org/wiki/Dusk#Technical_definitions
+	# see: https://upload.wikimedia.org/wikipedia/commons/d/d2/Twilight_subcategories.svg
+
+#############################################################
+# 🌇 ⤓⭱ 🌅🔅  ☼
+
+
+# switch between different content:
+# example:
+#   I set my panel applet to run this script every 3sec
+if [ "$(( `date '+%S'` % 2 ))" -eq 0 ] #A:
+then
+
+	# sunrise:
+	echo "🌅`sunwait list 1 rise angle $SUN_ANGLE $COORDINATES`"
+
+	if [[ `sunwait poll exit angle $SUN_ANGLE $COORDINATES` -eq "DAY" ]]
+	then # DAY
+		# countdown till sunset ⭳ :
+		date -u -d @$(($(date -d "`sunwait list 1 set 	angle $SUN_ANGLE $COORDINATES`" '+%s') - $(date -d "now" '+%s'))) '+🌇%-Hh%M'
+	else # NIGHT:
+		# countdown till sunrise ⭱:
+		date -u -d @$(($(date -d "`sunwait list 1 rise 	angle $SUN_ANGLE $COORDINATES`" '+%s') - $(date -d "now" '+%s'))) '+🌅%-Hh%M'
+	fi
+
+	# sunset:
+	echo "🌇`sunwait list 1 set angle $SUN_ANGLE $COORDINATES`"
+else # B:
+
+
+	# midday:
+	echo "🔅`sunwait report 1 			 $COORDINATES | grep 'Sun directly north/south' | sed 's/   Sun directly north\/south: //' | sed 's/ //'`"
+	
+	# day length:
+	echo "⏳`sunwait report 1 			 $COORDINATES | grep Duration | sed 's/ Duration ...    Day length: //' | sed 's/ hours//' | sed 's/:/h/'`"
+
+	# civil sunset:
+	echo "🕮`sunwait list 1 set civil $COORDINATES`"
+	
+
+	# 5h from sunset:
+	#echo "☭`sunwait list 1 set angle $SUN_ANGLE  offset 5:0:0 $COORDINATES`"
+fi
+
+
+
+
